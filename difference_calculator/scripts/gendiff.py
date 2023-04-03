7597c41a58eea9588e77d379ce5b021b55e17ee0
Exteni <Extenit@yandex.ru>
2023-04-03 22:10:39 +0300

    update script gendiff.py

--- difference_calculator/scripts/gendiff.py
+++ difference_calculator/scripts/gendiff.py
@@ -4,6 +4,9 @@ import argparse
 def main():
     diff = argparse.ArgumentParser("gendiff", 
             description="Compares two configuration files and shows a difference.")
+    diff.add_argument("-f", "--format",
+                      metavar="FORMAT", 
+                      help="set format of output")
     diff.add_argument("first_file")
     diff.add_argument("second_file")
     diff.parse_args()
