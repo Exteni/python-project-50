0eeb64961f0d7fdc6b25d6ae7701e3fa8e8f0939
Exteni <Extenit@yandex.ru>
2023-04-04 21:44:43 +0300

    add setup.cfg, flake8 and Maintainability badge from CodeClimate

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Exteni/python-project-50/workflows/hexlet-check/badge.svg)](https://github.com/Exteni/python-project-50/actions)
+<a href="https://codeclimate.com/github/Exteni/python-project-50/maintainability"><img src="https://api.codeclimate.com/v1/badges/b86dccf5ecfdb235af18/maintainability" /></a>
--- difference_calculator/scripts/gendiff.py
+++ difference_calculator/scripts/gendiff.py
@@ -2,11 +2,16 @@ import argparse
 
 
 def main():
-    diff = argparse.ArgumentParser("gendiff", 
-            description="Compares two configuration files and shows a difference.")
-    diff.add_argument("-f", "--format",
-                      metavar="FORMAT", 
-                      help="set format of output")
+    diff = argparse.ArgumentParser(
+        "gendiff",
+        description="Compares two configuration files and shows a difference."
+    )
+    diff.add_argument(
+        "-f",
+        "--format",
+        metavar="FORMAT",
+        help="set format of output"
+    )
     diff.add_argument("first_file")
     diff.add_argument("second_file")
     diff.parse_args()
