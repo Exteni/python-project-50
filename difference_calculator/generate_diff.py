7dbeb5c7045f8d3bc3089808ea5f2c5aa1b3ff65
Exteni <Extenit@yandex.ru>
2023-04-17 20:11:28 +0300

    refactor generate_diff.py and add pytest to dependencies

--- difference_calculator/generate_diff.py
+++ difference_calculator/generate_diff.py
@@ -5,22 +5,23 @@ def generate_diff(file1_path, file2_path):
     with open(file1_path) as file1, open(file2_path) as file2:
         file1 = json.load(file1)
         file2 = json.load(file2)
-    sorted_diff = create_formatted_dict(file1, file2)
-    return json.dumps(sorted_diff, indent=2, separators=("", ": "))
+    formatted_diff = create_formatted_diff(file1, file2)
+    print(formatted_diff)
+    return json.dumps(formatted_diff, indent=2, separators=("", ": "))
 
 
-def create_formatted_dict(dict1, dict2):
-    sorted_keys = sorted(set(dict1) | set(dict2))
+def create_formatted_diff(file1, file2):
+    sorted_keys = sorted(set(file1) | set(file2))
     formatted_dict = {}
     for key in sorted_keys:
-        if key in dict1 and key in dict2 and key not in formatted_dict:
-            if dict1[key] == dict2[key]:
-                formatted_dict[f"  {key}"] = dict1[key]
+        if key not in formatted_dict and key in file1 and key in file2:
+            if file1[key] == file2[key]:
+                formatted_dict[f"  {key}"] = file1[key]
             else:
-                formatted_dict[f"- {key}"] = dict1[key]
-                formatted_dict[f"+ {key}"] = dict2[key]
-        elif key in dict1:
-            formatted_dict[f"- {key}"] = dict1[key]
+                formatted_dict[f"- {key}"] = file1[key]
+                formatted_dict[f"+ {key}"] = file2[key]
+        elif key in file1:
+            formatted_dict[f"- {key}"] = file1[key]
         else:
-            formatted_dict[f"+ {key}"] = dict2[key]
+            formatted_dict[f"+ {key}"] = file2[key]
     return formatted_dict
